57c8d90dc87addec4fcb6373e96cfa5b109ffefd
user 112 <you@example.com>
2018-11-16 15:07:06 +0400

    Add Lab_3

---
+++ CourseApp/City.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CourseApp
+{
+    class City
+    {
+        public string Country { get; set; }
+        private int population;
+        public string Name { get; set; }
+
+        public int Population
+        {
+            get
+            {
+                return population;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.population = value;
+                }
+
+            }
+        }
+
+        public City() { Name = "Иваново"; Country = "Россия"; population = 316; }      // 1 конструктор
+
+        public City(string n, string m) { Name = n; Country = m; population = 11000; }         // 2 конструктор
+
+        public City(string n, string m, int a) { Name = n; Country = m; population = a; }     // 3 конструктор
+        public void GetInfo()
+        {
+            Console.WriteLine($"Название: {Name}  Страна: {Country}  Популяция: {population}");
+        }
+        public void s(int pop)
+        {
+            population += pop;
+        }
+        public void Ueh()
+        {
+            Console.WriteLine($"Из-за ужасной ядерной катастрофы всё население Иванова вымерло");
+        }
+
+    }
+}
--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -7,6 +7,30 @@ namespace CourseApp
         static void Main(string[] args)
         {
             Console.WriteLine("Анастасия Зайцева");
+            City ivanovo = new City();          // вызов 1-ого
+            City moscow = new City("Москва", "Россия");     // вызов 2-ого
+            City peter = new City("Питер", "Россия", 9000);  // вызов 3-ого
+
+
+            ivanovo.GetInfo();
+
+            ivanovo.Population = 5;
+            Console.WriteLine(ivanovo.Name);
+            ivanovo.GetInfo();
+            ivanovo.Population = -1;
+
+            ivanovo.GetInfo();
+
+            moscow.GetInfo();
+            peter.GetInfo();
+
+
+            ivanovo.s(100);
+            ivanovo.GetInfo();
+
+            ivanovo.Ueh();
+
+            Console.Read();
         }
     }
 }
