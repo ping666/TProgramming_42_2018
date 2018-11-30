ee71a4c5728a4287b6f2c962e43b7845912c6765
user 112 <you@example.com>
2018-11-30 14:18:11 +0400

    1 laba and all tests

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -2,11 +2,36 @@
 
 namespace CourseApp
 {
-    class Program
+   public class Program
     {
+        public static double formula(double x, double a, double b)
+        {
+            return (a + ((Math.Sin(b * x)) * (Math.Sin(b * x))) / ((Math.Cos(b * x)) * (Math.Cos(b * x)))) / (b + ((Math.Cos(a * x) * (Math.Cos(a * x)) / (Math.Sin(a * x) * (Math.Sin(a * x))))));
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Анастасия Зайцева");
+            Console.WriteLine("вар 17");
+            Console.WriteLine();
+            Console.WriteLine("под А");
+            double a = 0.1;
+            double b = 0.5;
+            double xn = 0.15;
+            double xk = 1.37;
+            double dx = 0.25;
+            for (double x = xn; x <= xk; x = x + dx)
+            {
+                Console.WriteLine($" x={x} y={Math.Round(formula(x, a, b), 3)}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("под B");
+            double[] z = new double[] { 0.2, 0.3, 0.44, 0.6, 0.56 };
+            foreach (double element in z)
+            {
+                Console.WriteLine($" X={element} y={Math.Round(formula(element, a, b), 3)} ");
+            }
+
             City ivanovo = new City();          // вызов 1-ого
             City moscow = new City("Москва", "Россия");     // вызов 2-ого
             City peter = new City("Питер", "Россия", 9000);  // вызов 3-ого
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -17,5 +17,39 @@ namespace test
             Assert.Equal(316, res_population);
             Assert.Equal("Иваново", res_name);
         }
+        [Fact]
+        public void Test2()
+        {
+            City moscow = new City("Москва","Россия");
+            var res_country = moscow.Country;
+            var res_population = moscow.Population;
+            var res_name = moscow.Name;
+            Assert.Equal("Россия", res_country);
+            Assert.Equal(11000, res_population);
+            Assert.Equal("Москва", res_name);
+        }
+        [Fact]
+        public void Test3()
+        {
+            City spb = new City("Питер", "Россия", 9000);
+            var res_country = spb.Country;
+            var res_population = spb.Population;
+            var res_name = spb.Name;
+            Assert.Equal("Россия", res_country);
+            Assert.Equal(9000, res_population);
+            Assert.Equal("Питер", res_name);
+        }
+             [Fact]
+        public void Test4()
+        {
+            var res = Program.formula(0.1, 0.5, 0.15);
+            Assert.Equal(0.001, res, 3);
+        }
+        [Fact]
+        public void Test5()
+        {
+            var res = Program.formula(0.0, 0.0, 0.1);
+            Assert.Equal(0.0, res, 3);
+        }
     }
 }
